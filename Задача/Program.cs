f41aab874c943e51d91077667b4b9d7d4ea71313
Pavel <asercrut@yanex.ru>
2023-02-01 10:18:22 +0400

    Programs go to end

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,2 +1,24 @@
 ﻿string[] ArrayOne = new string[4] {"Hello","2","world",":)-"}; // созадли исходный массив
 string[] ArrayTwo = new string[ArrayOne.Length]; // создали масив для вывода с длиной первого массива
+int count = 0; // создали переменную для записи
+
+void CopuCut(string[] ArrayOne, string[] ArrayTwo)
+{
+    int count= 0;
+    for (int i = 0; i < ArrayOne.Length; i++) // проходим по 1 массиву
+    {
+        if(ArrayOne[i].Length<=3) // если длина первого значения массива меньше или ровна трем символам, то записываем в масссив для вывода
+        {
+            ArrayTwo[count]=ArrayOne[i];
+            count++; // Запись в массив ArrayTwo
+        }
+    }
+}
+
+void Print(string[] Array)
+{
+    for (int i = 0; i < array.length; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+}
