6c35b762b64592d75f3c61713634a6c6c8e27f8e
Pavel <asercrut@yanex.ru>
2023-02-01 10:08:38 +0400

    Start programs

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ArrayOne = new string[4] {"Hello","2","world",":)-"}; // созадли исходный массив
+string[] ArrayTwo = new string[ArrayOne.Length]; // создали масив для вывода с длиной первого массива
